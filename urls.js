fc938340590c3f9de800ed3889c4545ae6071cd2
Ashier de Leon <ashier@gmail.com>
2013-06-02 15:54:25 +0800

    Updates:
    Added CRUD handling for request based on routes

--- urls.js
+++ urls.js
@@ -7,6 +7,19 @@
     'use strict';
 
     module.exports = function() {
+        // ========================================
+        // URL_PATTERNS
+        //  Jumper parses these patterns to handle routing.
+        //
+        // paramaters:
+        //
+        // route
+        //  - url routes
+        // views
+        //  - app . filename . method
+        // method
+        //  - get | post | put | delete
+        // ========================================
         return {
             URL_PATTERNS: [
                 // Uncomment the next line to enable the admin:
