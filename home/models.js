ce90743a339eea42cacb983f7c1c5034c3ec6b99
Ashier de Leon <ashier@gmail.com>
2013-06-10 22:09:20 +0800

    Update Model Implementation

--- home/models.js
+++ home/models.js
@@ -1,13 +1,38 @@
-var Model = require('jumper.js').Model;
+var Model = require('jumper.js').Model,
+    utils = require('jumper.js').utils;
 
-var Post = {
+
+var User = {
 
     attributes: {
-
+        first_name: Model.STRING,
+        last_name: Model.STRING
     }
 
 };
 
-console.log(Model);
+var Post = {
+
+    attributes: {
+        author: User,
+        title: Model.STRING,
+        contents: Model.STRING,
+        created: {
+            type:Model.DATE,
+            default:new Date()
+        }
+    },
+
+    pre: {
+        'save': function(err, model) {
+            // handle error ok ?
+            console.log('calling pre save', model);
+            model.slug = utils.slugify(model.title);
+        }
+    }
+
+};
+
+// console.log('> ', utils.slugify('ashier de leon'));
 
 module.exports.Post = Post;
--- home/views.js
+++ home/views.js
@@ -1,3 +1,4 @@
+var Post = require('./models').Post;
 
 module.exports.index = function(req, res) {
     res.render('../templates/home/index', {message:"A simple message from Jumper Web Framework"});
@@ -7,18 +8,5 @@ module.exports.login = function(req, res) {
     res.send('login page');
 };
 
-// console.log("Post : ", Post.description);
-
-// // Listen to db's event 'connected'
-// db.connection.on('connected', function(err) {
-//     if (err) throw err;
-//     // DB Connected
-//     console.log("Post : ", Post.description);
-// });
-
-
-// module.exports.index = function(req, res) {
-//     res.render(path.join(__dirname, '..', '/templates/home/index.jade'));
-// };
-
-
+// var post = Post.create({title:'My Title'});
+// Post.pre.save(post);
