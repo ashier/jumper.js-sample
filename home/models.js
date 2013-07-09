b04caa4ee8c105bc28bb72f2d6e9d1141a90132a
Ashier de Leon <ashier@gmail.com>
2013-07-10 03:39:31 +0800

    Best Coding Practices: 2 Spaces

--- home/models.js
+++ home/models.js
@@ -5,36 +5,36 @@
  */
 
 var j = require('jumper.js'),
-    Model = j.Model,
-    utils = j.utils;
+  Model = j.Model,
+  utils = j.utils;
 
 var User = Model.extend({
 
-    attributes: {
-        first_name: Model.STRING,
-        last_name: Model.STRING
-    }
+  attributes: {
+    first_name: Model.STRING,
+    last_name: Model.STRING
+  }
 
 });
 
 var Post = Model.extend({
 
-    attributes: {
-        author: User,
-        title: Model.STRING,
-        contents: Model.STRING,
-        created: {
-            type:Model.DATE,
-            default:new Date()
-        }
-    },
-
-    pre: {
-        'save': function(err, model) {
-            console.log('calling pre save', model);
-            model.slug = utils.slugify(model.title);
-        }
+  attributes: {
+    author: User,
+    title: Model.STRING,
+    contents: Model.STRING,
+    created: {
+      type: Model.DATE,
+      default: new Date()
     }
+  },
+
+  pre: {
+    'save': function(err, model) {
+      console.log('calling pre save', model);
+      model.slug = utils.slugify(model.title);
+    }
+  }
 
 });
 
--- home/views.js
+++ home/views.js
@@ -7,11 +7,15 @@
 var Post = require('./models').Post;
 
 module.exports.index = function(req, res) {
-    res.render('../templates/home/index', {message:"A simple message from Jumper Web Framework"});
+  res.render('../templates/home/index', {
+    message: "A simple message from Jumper Web Framework"
+  });
 };
 
 module.exports.login = function(req, res) {
-    res.send('login page');
+  res.send('login page');
 };
 
-Post.save({title:'My Title'});
+Post.save({
+  title: 'My Title'
+});
